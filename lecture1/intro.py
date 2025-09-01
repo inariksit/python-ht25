e91c415ce61a7d7c076bf9de7651da6a647cb9d2
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-01 09:37:50 +0200

    Add västtrafik example

--- lecture1/intro.py
+++ lecture1/intro.py
@@ -1,1 +1,5 @@
-print("Hello, world!")
+vasttrafik_enkelbiljett_pris = 37
+resor = int(input("Hur många gånger reser du idag? "))
+
+total = resor * vasttrafik_enkelbiljett_pris
+print("Du reser", resor, "gånger och det kostar", total, "kr")
