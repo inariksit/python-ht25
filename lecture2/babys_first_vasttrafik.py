bd4f809fc14129ce0e56d4d26a2da6a1c7334ea9
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-04 08:46:54 +0200

    Add first version that only uses variables and mathematical operations

--- lecture2/babys_first_vasttrafik.py
+++ lecture2/babys_first_vasttrafik.py
@@ -1,3 +1,25 @@
 ## To be filled in class
 
 ## "Hur mycket behöver jag resa så att det är värt att köpa dygnsbiljett/månadskort"?
+
+# Studentpriser
+enkel = 28
+dygn = 90
+tredygn = 180
+månad = 645
+
+
+### Beräkningar
+värt_med_dygn = int(dygn / enkel) + 1
+print("Värt att köpa dygnsbiljett om man reser minst", värt_med_dygn,"gånger under ett dygn")
+
+print("Eftersom:", (värt_med_dygn - 1) * enkel, "kostar mindre än", dygn)
+print("Eftersom:", värt_med_dygn * enkel, "kostar mer än", dygn)
+
+
+#####
+
+värt_med_tredygn = int(tredygn / enkel) + 1
+print("Värt att köpa tredygnsbiljett om man reser minst", värt_med_tredygn, "gånger under tre dygn")
+
+
