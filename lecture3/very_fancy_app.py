3b4e62d01ab8c08feda29534f196f10d48fc1027
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-08 14:50:31 +0200

    make fancy_print a bit less fancy

--- lecture3/very_fancy_app.py
+++ lecture3/very_fancy_app.py
@@ -1,14 +1,14 @@
 import price
 
 ## Helper function to print in fancy colors
-def fancy_print(color, *args, **kwargs):
-    colored = color + ' '.join(str(a) for a in args) + "\033[0m"
-    print(colored, **kwargs)
+def color_print(color, arg):
+    # Coercing arg with str() just in case
+    print(color + str(arg) + "\033[0m")
 
 # Will print the price progression
 # with every year in different color!
 vt_price = 37
 for year in range(2025,2031):
   color = f"\033[{1};{year-1994};{40}m"
-  fancy_print(color, f"{year}: {vt_price:.2f}")
+  color_print(color, f"{year}: {vt_price:.2f}")
   vt_price = price.increment(vt_price, 2.6)
