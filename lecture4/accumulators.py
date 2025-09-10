dec8a513a7ba25c557d414f54717ea7562dddcb4
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-10 22:32:58 +0200

    add stubs for live coding tomorrow

---
+++ lecture4/accumulators.py
@@ -0,0 +1,14 @@
+## More examples of the accumulator pattern
+
+#################################################################
+# Return the highest and lowest number. Don't use min() or max().
+# Input: [8, 5.5, 10, 6, 9, 9.5, 2.0]
+# Expected result: [10, 2.0]
+
+
+
+################################################################
+# Return how many numbers in the list are negative.
+# Input: [-10.5, 5.5, -2.5, 1, 0, 12, -14.8]
+# Expected result: 3
+
---
+++ lecture4/convert_bases.py
@@ -0,0 +1,9 @@
+## Take a numeric notation in any base between 2-10 and compute its value.
+# Examples:
+# 235 in decimal:
+#    2 · 10² + 3 · 10¹ + 5 · 10⁰
+# =  200 + 30 + 5
+# 235 in nonary:
+#    2 · 9² + 3 · 9¹ + 5 · 9⁰
+# =  162 + 27 + 5
+# =  194
