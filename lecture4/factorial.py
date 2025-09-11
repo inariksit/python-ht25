db647c0976cf41bf0087810716af827b304ffb63
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-11 08:46:12 +0200

    add factorial that uses floats

--- lecture4/factorial.py
+++ lecture4/factorial.py
@@ -1,8 +1,16 @@
 def factorial(n):
   fac = 1
   for k in range(2, n+1):
-    print("k=", k, "fac=", fac)
+    #print("k=", k, "fac=", fac)
     fac = fac * k      # alt: fac *= k
+  return fac
+
+def factorial_float(n):
+  fac = 1.0 # initialize accumulator to a float
+
+  # k cannot be float: range only takes integers
+  for k in range(2, n+1):
+    fac = fac * k
   return fac
 
 if __name__=='__main__':
