0719ad38148eb6d455791f672b506f025c278bc6
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-11 09:45:37 +0200

    filled out base conversion function

--- lecture4/convert_bases.py
+++ lecture4/convert_bases.py
@@ -7,3 +7,22 @@
 #    2 · 9² + 3 · 9¹ + 5 · 9⁰
 # =  162 + 27 + 5
 # =  194
+
+def convert(notation, base):
+    notation_str = str(notation)
+    notation_rev = reversed(notation_str)
+
+    acc = 0
+    # Will explain "for x, y in …" Monday's lecture!
+    for power, char in enumerate(notation_rev):
+        digit = int(char)
+
+        # if digit belongs to base
+        if digit < base:
+            acc += digit * (base ** power)
+        else:
+            print("Digit", digit, "too large for base", base)
+            return # returns nothing, exits function
+
+    return acc
+
