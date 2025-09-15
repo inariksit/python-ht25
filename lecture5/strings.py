a89043534200786d78918a33aa7a3fd815fb56c1
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-15 08:08:40 +0200

    printing with sep and end

--- lecture5/strings.py
+++ lecture5/strings.py
@@ -1,7 +1,11 @@
 ## String literals
 ## Printing, formatting
+name = input("Your name?\n> ")
+uni = input("Your university?\n> ")
+print(name, "@", uni, ".se", sep="^_^")
 
-
+for _ in range(5):
+    print("test", end="^_^")
 
 
 ## String operations: concat, repeat, index, slice, length, iteration, membership
