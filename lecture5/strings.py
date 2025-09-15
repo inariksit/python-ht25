e067c11c07dc06174a4e294a6391d4c0baf29354
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-15 08:43:52 +0200

    add string operations

--- lecture5/strings.py
+++ lecture5/strings.py
@@ -7,8 +7,62 @@ print(name, "@", uni, ".se", sep="^_^")
 for _ in range(5):
     print("test", end="^_^")
 
+## Unicode: code points
+print(ord('A'))
 
-## String operations: concat, repeat, index, slice, length, iteration, membership
+
+## String operations:
+# concat
+name = "inari"
+uni = "chalmers"
+email = name + "@" + uni + ".se"
+print(email)
+
+# repeat
+example = 3 * uni
+print(example)
+
+# index
+a = uni[2]
+print(a)
+
+# uni[10000]
+# IndexError: string index out of range
+
+# slice: string[start:stop]
+alm = uni[2:5]
+print(alm)
+
+# -1 is the last character
+# slicing is exclusive
+almer = uni[2:-1]
+print(almer)
+
+# 2 ways to get "almers"
+almers1 = uni[2:]
+almers2 = uni[2:10000]
+print(almers1, almers2)
+
+chal = uni[:4]
+mers = uni[4:]
+print(chal, mers)
+
+
+# length & iteration
+print(len(uni))
+
+# index loop: loop over indices
+for i in range( len(uni) ):
+    print(i, uni[i])
+
+# foreach: loop over elements
+for c in uni:
+    print(c, "code point:", ord(c))
+
+# membership
+print('s' in uni)
+print('cha' in uni)
+print('c h a' in uni)
 
 
 
