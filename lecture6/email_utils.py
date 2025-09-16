2af3bcbe6eca599d81d8f5c16c6fc5113d80acf5
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-16 14:36:37 +0200

    for Thursday lecture: lists and tuples cont'd

---
+++ lecture6/email_utils.py
@@ -0,0 +1,24 @@
+## Small utility module for sending email https://docs.python.org/3/library/smtplib.html
+
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email(subject, msg_raw, recipient):
+    # Gmail credentials
+    sender = "inari.listenmaa@gmail.com"
+    password = "DON'T PUBLISH ACTUAL PASSWORDS ON THE INTERNET AS PLAINTEXT"
+    # If you want your own: create one in https://myaccount.google.com/apppasswords
+    # and please don't post it in the public internet 🙈
+
+    # Create the email
+    msg = MIMEText(str(msg_raw))
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
+
+    # Connect and send
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender, password)
+        server.send_message(msg)
+
+    print("Email sent successfully!")
---
+++ lecture6/lists_and_tuples.py
@@ -0,0 +1,10 @@
+from email_utils import send_email
+
+# A person is defined by their name, email address and city of residence
+ulrika = ("Ulrika", "uu@uu.uu", "Uddevalla")
+markus = ("Markus", "mm@mm.mm", "Mölndal")
+karl = ("karl", "kk@kk.kk", "Kungälv")
+pia = ("Pia", "pp@pp.pp", "Partille")
+inari = ("Inari", "inari@chalmers.se", "Göteborg")
+
+# Loop over the people, send email to people who live in Göteborg
