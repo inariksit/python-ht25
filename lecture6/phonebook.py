5f3e6decd678dac15638585e7cdb119aa54eb895
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-09-17 20:31:16 +0200

    add plan for live coding

---
+++ lecture6/phonebook.py
@@ -0,0 +1,9 @@
+## Read contacts from the file people.txt into a dictionary
+# Alice: 1234567890
+# Bob: 9876543210
+
+## Add a new contact:
+# Vaktmästare: 0317728800
+
+## Print updated phonebook into a new file, in the same format
+
