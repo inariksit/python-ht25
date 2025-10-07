1f1a9b323bcb41fea5f691e5186391b486f085ff
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-10-07 17:13:42 +0200

    add skeleton for polynomial search algorithms

---
+++ lecture12/polynomial.py
@@ -0,0 +1,35 @@
+# import random
+# haystack = [random.randrange(-30,100) for _ in range(20)]
+# needle = random.randrange(-100,100)
+
+haystack = [15, -8, 23, 7, -12, 31, 4, -3, 19, 11, -5, 28, 2, -15, 9, 6, -20, 14, 1, 25]
+needle = 13
+
+#####################################################################
+## Find a specific value ('needle') from a collection ('haystack') ##
+#####################################################################
+
+def search(needle, haystack):
+  # return index of needle if found
+  # return -1 if not found
+  pass
+
+#########################################################################
+## Is there a pair of values in haystack whose sum is equal to needle? ##
+#########################################################################
+
+def pair_sum(needle, haystack):
+  # return indices of straws if found
+  # return (-1,-1) if not found
+  pass
+
+
+###########################################################################
+## Is there a triple of straws in haystack whose sum is equal to needle? ##
+###########################################################################
+
+def triple_sum(needle, haystack):
+  # return indices of straws if found
+  # return (-1,-1,-1) if not found
+  pass
+
